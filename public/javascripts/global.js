433d17bfd648d2bfd24b5705f16bdef9594a74e6
mindrudan <mindrudan@gmail.com>
2014-03-05 00:24:42 +0100

    added new files

---
+++ public/javascripts/global.js
@@ -0,0 +1,33 @@
+var userListData = [];
+
+$(document).ready(function(){
+	loadUsers();
+});
+
+function loadUsers(){
+	var userContent = '<hr/> <strong>MongoDB/mongoose</strong> says ';
+
+	$.getJSON('/users', function(data){
+		userListData = data;
+		var intermediateContent = '';
+		var userNumber = 0;
+
+		$.each(data, function(){
+			intermediateContent += '<span class="a-name"><i>'+this.Name+'</i></span>';
+
+			userNumber ++;
+		});
+
+		if(userNumber === 0){
+			userContent += 'there are currently no users.'
+		}
+		else if(userNumber === 1){
+			userContent += 'there is currently <strong>1</strong> user registered:<br/>'
+		}
+		else{
+			userContent += 'there are currently <strong>'+userNumber+'</strong> users registered:<br/>'
+		}
+
+		$('#userList').html(userContent+intermediateContent);
+	});
+}
