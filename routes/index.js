b722ead453e88bf1d2c46b530cb798df68b4524e
mindrudan <mindrudan@gmail.com>
2014-03-04 20:16:25 +0100

    changed index

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'mindrudan.com will change to an <strong>express</strong> app.' });
+  res.render('index', { title: 'mindrudan.com will change to an Express app.' });
 };
